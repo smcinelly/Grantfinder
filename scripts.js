83c3f0b80c26ccbb8fc97e28407503d778e3268c
Kate <k8hansen@gmail.com>
2019-07-09 10:30:21 -0400

    added pages for each card and javascript action to make entire cards clickable and linked

--- scripts.js
+++ scripts.js
@@ -5,3 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
   });
   M.AutoInit();
 
+  $(document).ready(() => {
+    $(document.body).on('click', '.card1[data-clickable=true]', (e) => {
+      var href = $(e.currentTarget).data('href');
+      window.location = href;
+    });
+  });
+
