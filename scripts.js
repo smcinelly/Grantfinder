5bec250bae0c15c4fa508cf824a797755db5c5e3
smac <stephaniemcinelly@gmail.com>
2019-07-12 00:48:50 -0700

    changes to script

--- scripts.js
+++ scripts.js
@@ -1,12 +1,6 @@
 
 
-// clickable card functionality
-$(document).ready(() => {
-  $(document.body).on('click', '.card1[data-clickable=true]', (e) => {
-    var href = $(e.currentTarget).data('href');
-    window.location = href;
-  });
-});
+
 
 
 
